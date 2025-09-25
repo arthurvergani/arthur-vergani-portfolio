0224205f1babb17fb870d0d60ad3f9ede97a6d22
Arthur Vergani <hello@arthurvergani.com>
2025-09-25 09:30:14 +0100

    changed path to styles in html

--- build.js
+++ build.js
@@ -65,8 +65,8 @@ function buildPortfolio() {
         templateHead = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../src/variables.css">
-    <link rel="stylesheet" href="../src/styles.css">
+<link rel="stylesheet" href="variables.css">
+<link rel="stylesheet" href="styles.css">
     <title>Portfolio</title>`;
     }
 
